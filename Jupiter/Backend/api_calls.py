0f7abddcbb96672f31177768a890cc5c8309c23d
FabioDiGrillo <9120@htl.rennweg.at>
2023-10-03 10:42:08 +0200

    created api_calls file api key inside

---
+++ Jupiter/Backend/api_calls.py
@@ -0,0 +1,19 @@
+"""
+Fabio Di Grillo
+03.10.2023
+"""
+
+
+import requests
+
+
+apiKey = "C6KzQwff39MA8kV1"
+
+def get_forecast (apikey,lat_long):
+    return requests.get(f"https://api.pirateweather.net/forecast//")
+
+
+
+
+
+print(get_forecast(apiKey))
