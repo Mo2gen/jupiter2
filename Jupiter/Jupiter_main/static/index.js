49c6fbd5c10a84e7d4b5c86de555047171d88617
Christoph Allmer <christoph.allmer@htl.rennweg.at>
2023-10-10 13:06:05 +0200

    geodecoding function implemented

--- Jupiter/Jupiter_main/static/index.js
+++ Jupiter/Jupiter_main/static/index.js
@@ -9,6 +9,23 @@ function getCoordinates() {
 }
 
 function handlecoordinates(myposition) {
-    console.log("Your Latitude is: " + myposition.coords.latitude)
-    console.log("Your Longitude is: " + myposition.coords.longitude)
+    // hier get request schicken
+}
+
+function geocodeCity(cityName) {
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({ address: cityName }, function (results, status) {
+    if (status === "OK") {
+      if (results[0]) {
+        var location = results[0].geometry.location;
+        console.log("Latitude: " + location.lat());
+        console.log("Longitude: " + location.lng());
+      } else {
+        console.log("No results found");
+      }
+    } else {
+      console.log("Geocode was not successful for the following reason: " + status);
+    }
+  });
 }
--- Jupiter/Jupiter_main/views.py
+++ Jupiter/Jupiter_main/views.py
@@ -1,10 +1,12 @@
+import random
+
 from django.template import loader
 from django.shortcuts import render
 from Jupiter_Backend import api_calls
 
 def index(request):
     context = {
-        "currentTemp": 833
+        "currentTemp": random.randint(-10, 40)
     }
     # api_calls.test()
     return render(request, 'index.html', context)
