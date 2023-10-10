21a7bd9dc1888fd78265b71e6c17ac76a45690be
FabioDiGrillo <9120@htl.rennweg.at>
2023-10-10 10:37:37 +0200

    added convert timestamp function

--- Jupiter/Backend/api_calls.py
+++ Jupiter/Backend/api_calls.py
@@ -2,21 +2,23 @@
 Fabio Di Grillo
 03.10.2023
 """
-
+import datetime
 import time
+
 import requests
 
 # Wichtig! Ohne diesen wird kein Zugirff erlaubt
 apiKey = "C6KzQwff39MA8kV1"
 
-def get_forecast_json (apikey,lat,long ):
+def get_forecast_json(apikey, lat, long):
     """
     make API call to pirateweather, forcast for latitude and longitude
     -> return response as json, save timestamp of request
     lat: latitude
     long: longitude
     """
-    return requests.get(f"https://api.pirateweather.net/forecast/{apikey}/{lat},{long}").json()
+    return requests.get(f"https://api.pirateweather.net/forecast/{apikey}/{lat},{long}?units=si").json()
+
 
 def save_forecast(forecast):
     """
@@ -24,8 +26,9 @@ def save_forecast(forecast):
     """
 
     forecast_request = {
-        "PK_timestamp" : time.time(),
-        "CurrentTemperature" : forecast["currently"]["temperature"]
+        "PK_timestamp": time.time(),
+        "normaltime": convert_timestamp_normaltime(time.time()),
+        "CurrentTemperature": forecast["currently"]["temperature"]
     }
 
     forcast_hours = []
@@ -33,13 +36,19 @@ def save_forecast(forecast):
     for h in forecast["hourly"]["data"]:
         forecast_hour = {
             "PK_timestamp": time.time(),
-            "timestamphour":  h["time"],
+            "timestamphour": h["time"],
+            "normaltime": convert_timestamp_normaltime(h["time"]),
             "Temperature": h["temperature"]
         }
         forcast_hours.append(forecast_hour)
 
-    return forecast_request,forcast_hours
+    return forecast_request, forcast_hours
 
+
+def convert_timestamp_normaltime(t):
+    """
+
+    """
+    return datetime.datetime.fromtimestamp(t).strftime("%Y-%m-%d %H:%M")
 
 print(save_forecast(get_forecast_json(apiKey, "48.210033", "16.363449")))
-
--- Jupiter/Jupiter_main/static/index.js
+++ Jupiter/Jupiter_main/static/index.js
@@ -1,4 +1,3 @@
-
 function startfun(){
     getCoordinates()
 }
