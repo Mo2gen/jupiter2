e4c32c2805de5bc772974715f7be321b3b73f979
Christoph Allmer <christoph.allmer@htl.rennweg.at>
2023-10-10 13:42:00 +0200

    Umwandlung von Koordinaten auf Stadtnamen

--- Jupiter/Jupiter_main/static/index.js
+++ Jupiter/Jupiter_main/static/index.js
@@ -9,10 +9,11 @@ function getCoordinates() {
 }
 
 function handlecoordinates(myposition) {
-    // hier get request schicken
+    console.log("Latitude: " + myposition.coords.latitude)
+    console.log("Longitude: " + myposition.coords.longitude)
 }
 
-function geocodeCity(cityName) {
+function getCityCoords(cityName) {
   var geocoder = new google.maps.Geocoder();
 
   geocoder.geocode({ address: cityName }, function (results, status) {
@@ -21,11 +22,32 @@ function geocodeCity(cityName) {
         var location = results[0].geometry.location;
         console.log("Latitude: " + location.lat());
         console.log("Longitude: " + location.lng());
+      }
+    }
+  });
+}
+
+function getCityName(lat, lng) {
+  var geocoder = new google.maps.Geocoder();
+  var latlng = { lat: parseFloat(lat), lng: parseFloat(lng) };
+
+  geocoder.geocode({ 'location': latlng }, function (results, status) {
+    if (status === 'OK') {
+      if (results[0]) {
+        // Loop through address components to find the city
+        for (var i = 0; i < results[0].address_components.length; i++) {
+          var component = results[0].address_components[i];
+          if (component.types.includes('locality')) {
+            var cityName = component.long_name;
+            console.log('City Name: ' + cityName);
+            break; // Stop looping once the city name is found
+          }
+        }
       } else {
-        console.log("No results found");
+        console.log('No results found');
       }
     } else {
-      console.log("Geocode was not successful for the following reason: " + status);
+      console.log('Geocoder failed due to: ' + status);
     }
   });
 }
