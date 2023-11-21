5c5d959b258eef9a16ed6df168cd88d8e685dc68
Alex <9138@htl.rennweg.at>
2023-11-21 12:23:23 +0100

    weather icons broken

--- Jupiter/Jupiter_main/views.py
+++ Jupiter/Jupiter_main/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from Jupiter_Backend import api_calls
 
 def index(request):
     context = {
         "currentTemp": 8
     }
-    api_calls.getandsave()
     return render(request, 'index.html', context)
