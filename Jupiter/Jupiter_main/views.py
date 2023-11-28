f84be8aa8a1195b2af79d5decda11c3a1839995f
Christoph Allmer <christoph.allmer@htl.rennweg.at>
2023-11-28 13:56:17 +0100

    graph leicht broken

--- Jupiter/Jupiter_main/views.py
+++ Jupiter/Jupiter_main/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render
 from datetime import datetime
 from Jupiter_Backend.models import ForecastHour, ForecastRequest
+import json
+
+# Fick dich
 icons = {
     'clear': '',
     'cloudy': '',
+    'partly cloudy': '',
     'rain': '',
     'thunderstorm': '',
     'foggy': '',
     'windy': '',
-    'snowing': '',
-    'solar-eclipse': ''
+    'snow': '',
+    'solar-eclipse': '',
+    'hail': ''
 }
 
+
 def index(request):
-    print(request.COOKIES.get('long'))
-    print(request.COOKIES.get('lat'))
-    print(request.COOKIES.get('date'))
-    for a in ForecastRequest.objects.values():
-        print(a)
-        print(datetime.fromtimestamp(a['pk_timestamp']).date())
-        if request.COOKIES.get('date') == datetime.fromtimestamp(a['pk_timestamp']).date():
-            print('found!!!!!!!!!!!!!!!!!!')
     context = {
         "currentTemp": 8,
         "wind": 0,
@@ -30,7 +28,23 @@ def index(request):
         "min": 0,
         "max": 0,
         "weather": icons['clear'],
-        "date": datetime.now().strftime('%A %H:%M')
+        "date": datetime.now().strftime('%A %H:%M'),
+        'liste': dict
     }
+    temp = None
+    for a in ForecastRequest.objects.values():
+        if request.COOKIES.get('date') == datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d"):
+            temp = [
+                {
+                    'PK_timestamp': b['fk_timestamp_id'],
+                    'timestamphour': b['timestamphour'],
+                    'normaltime': b['normaltime'].strftime('%H:%M:%S'),
+                    'Temperature': b['temperature_cur'],
+                    'weather': icons[b['weathersummary'].lower()]
+                }
+                for b in ForecastHour.objects.values() if b['fk_timestamp_id'] == a['pk_timestamp']
+            ]
+    print(temp)
+    context['liste'] = json.dumps(temp)
     response = render(request, 'test.html', context)
     return response
