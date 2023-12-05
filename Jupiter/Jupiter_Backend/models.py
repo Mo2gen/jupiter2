e0bb426dfc9bea84800e1a986830b082370b50a7
Christoph Allmer <christoph.allmer@htl.rennweg.at>
2023-12-05 11:35:35 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Jupiter/Jupiter_Backend/models.py
+++ Jupiter/Jupiter_Backend/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class ForecastHour(models.Model):
-    pk_forecast_hour_id = models.AutoField(db_column='PK_forecast_hour_ID', primary_key=True)
-    fk_timestamp = models.ForeignKey('ForecastRequest', models.DO_NOTHING, db_column='FK_timestamp', blank=True, null=True)
-    timestamphour = models.IntegerField(db_column='timestamphour')
+    pk_forecast_hour_id = models.AutoField(db_column='PK_forecast_hour_ID', primary_key=True)  # Field name made lowercase.
+    fk_request = models.ForeignKey('ForecastRequest', models.DO_NOTHING, db_column='FK_request', blank=True, null=True)  # Field name made lowercase.
+    timestamphour = models.IntegerField(blank=True, null=True)
     temperature_cur = models.IntegerField(db_column='temperature_cur')
     humidity = models.FloatField(db_column='humidity')
-    windspeed = models.FloatField(db_column='windSpeed')
-    uvindex = models.IntegerField(db_column='uvIndex')
-    airpressure = models.IntegerField(db_column='airPressure')
-    weathersummary = models.CharField(db_column='weatherSummary', max_length=30)
-    normaltime = models.TimeField(db_column='normalTime')
+    windspeed = models.FloatField(db_column='windSpeed')  # Field name made lowercase.
+    uvindex = models.IntegerField(db_column='uvIndex')  # Field name made lowercase.
+    airpressure = models.IntegerField(db_column='airPressure')  # Field name made lowercase.
+    weathersummary = models.CharField(db_column='weatherSummary', max_length=30)  # Field name made lowercase.
+    normaltime = models.TimeField(db_column='normalTime')  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -19,7 +19,8 @@ class ForecastHour(models.Model):
 
 
 class ForecastRequest(models.Model):
-    pk_timestamp = models.IntegerField(db_column='PK_timestamp', primary_key=True)  # Field name made lowercase.
+    pk_forecast_id = models.AutoField(db_column='PK_forecast_ID', primary_key=True)  # Field name made lowercase.
+    pk_timestamp = models.IntegerField(db_column='PK_timestamp', blank=True, null=True)  # Field name made lowercase.
     latitude = models.FloatField(db_column='latitude')
     longitude = models.FloatField(db_column='longitude')
     currenttemperature = models.IntegerField(db_column='CurrentTemperature')  # Field name made lowercase.
--- Jupiter/Jupiter_Backend/serializer.py
+++ Jupiter/Jupiter_Backend/serializer.py
@@ -4,11 +4,11 @@ from .models import ForecastHour, ForecastRequest
 class ForecastHourSerializer(serializers.ModelSerializer):
     class Meta:
         model = ForecastHour
-        fields = ['pk_forecast_hour_id', 'fk_timestamp', 'timestamphour', 'temperature_cur', 'humidity', 'windspeed', 'uvindex', 'airpressure', 'weathersummary', 'normaltime']
+        fields = ['pk_forecast_hour_id', 'fk_request', 'timestamphour', 'temperature_cur', 'humidity', 'windspeed', 'uvindex', 'airpressure', 'weathersummary', 'normaltime']
 
 
 class ForecastRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = ForecastRequest
-        fields = ["pk_timestamp", 'latitude', 'longitude', "currenttemperature"]
+        fields = ["pk_forecast_id, pk_timestamp", 'latitude', 'longitude', "currenttemperature"]
 
