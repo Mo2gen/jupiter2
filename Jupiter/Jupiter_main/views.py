34294d35c15bce57ac845b3ba93b9b6955126571
Christoph Allmer <christoph.allmer@htl.rennweg.at>
2023-12-05 19:56:29 +0100

    Standort sollte jetzt gehen

--- Jupiter/Jupiter_main/views.py
+++ Jupiter/Jupiter_main/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from Jupiter_Backend.models import ForecastHour, ForecastRequest
 from Jupiter_Backend.api_calls import getandsave as getandsave
 import json
+import math
 
 icons = {
     'clear': '',
@@ -31,20 +32,22 @@ def index(request):
         date = datetime.now().strftime('%Y-%m-%d')
     else:
         date = request.COOKIES.get('date')
+    print(lat)
+    print(long)
     todayRequest = None
     for a in ForecastRequest.objects.values():
-        if datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d") == datetime.now().date().strftime("%Y-%m-%d") and a['latitude'] == round(lat, 3) and a['longitude'] == round(long, 4):
+        if datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d") == datetime.now().date().strftime("%Y-%m-%d") and math.isclose(lat, a['latitude'], abs_tol=0.009) and math.isclose(long, a['longitude'], abs_tol=0.009):
             todayRequest = a
     if not todayRequest:
         getandsave(lat, long, "now")
         for a in ForecastRequest.objects.values():
-            if datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d") == datetime.now().date().strftime("%Y-%m-%d") and a['latitude'] == round(lat, 3) and a['longitude'] == round(long, 4):
+            if datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d") == datetime.now().date().strftime("%Y-%m-%d") and math.isclose(lat, a['latitude'], abs_tol=0.009) and math.isclose(long, a['longitude'], abs_tol=0.009):
                 todayRequest = a
     today = [a for a in ForecastHour.objects.values() if a['fk_request_id'] == todayRequest['pk_forecast_id']]
     now = [a for a in today if a['normaltime'].strftime("%H") == datetime.now().strftime("%H")][0]
     context = {
         "currentTemp": now['temperature_cur'],
-        "wind": now['windspeed'] * 3.6,
+        "wind": round(now['windspeed'] * 3.6, 1),
         "humidity": now['humidity'] * 100,
         "uv": now['uvindex'],
         "pressure": now['airpressure'],
@@ -54,20 +57,19 @@ def index(request):
         "date": datetime.now().strftime('%A %H:%M'),
         'liste': dict
     }
-    temp = generatelist(date)
+    temp = generatelist(date, lat, long)
+    print(temp)
     if not temp:
         print(int(datetime.strptime(date, '%Y-%m-%d').timestamp()))
-        #getandsave(lat, long, int(datetime.strptime(date, '%Y-%m-%d').timestamp()))
-    #   temp = generatelist(date)
     context['liste'] = json.dumps(temp)
     response = render(request, 'test.html', context)
     return response
 
 
-def generatelist(date: str):
+def generatelist(date: str, lat, long):
     liste = None
     for a in ForecastRequest.objects.values():
-        if date == datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d"):
+        if date == datetime.fromtimestamp(a['pk_timestamp']).strftime("%Y-%m-%d") == datetime.now().date().strftime("%Y-%m-%d") and math.isclose(lat, a['latitude'], abs_tol=0.009) and math.isclose(long, a['longitude'], abs_tol=0.009):
             liste = [
                 {
                     'timestamphour': b['timestamphour'],
