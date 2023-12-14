6c9cea54601a8cc1da27be76286f02778c964eea
Mo2gen <superchrisi1308@gmail.com>
2023-12-14 12:48:50 +0100

    settings fix?

--- Jupiter/Jupiter/settings.py
+++ Jupiter/Jupiter/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "Jupiter",
         'USER': "root",
-        'PASSWORD' : 'root',
+        'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT' : '3306',
         'OPTIONS': {
